d1111b3829e6a4d62730de89ce8201bf76cf474a
Musa Hamwala II <musahamwala@icloud.com>
2019-11-09 21:13:47 +0000

    adding Staff Route (#3)

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -7,6 +7,7 @@ import { alertActions } from '../_actions';
 import { PrivateRoute } from '../_components';
 import { HomePage } from '../HomePage';
 import { LoginPage } from '../LoginPage';
+import { StaffPage } from '../StaffPage';
 
 class App extends React.Component {
     constructor(props) {
@@ -31,6 +32,7 @@ class App extends React.Component {
                             <Switch>
                                 <PrivateRoute exact path="/" component={HomePage} />
                                 <Route path="/login" component={LoginPage} />
+                                <Route path="/staff" component={StaffPage} />
                                 <Redirect from="*" to="/" />
                             </Switch>
                         </Router>
---
+++ src/StaffPage/StaffPage.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { userActions } from '../_actions';
+
+class StaffPage extends React.Component {
+    componentDidMount() {
+        this.props.getUsers();
+    }
+
+    handleDeleteUser(id) {
+        return (e) => this.props.deleteUser(id);
+    }
+
+    render() {
+        const { user, users } = this.props;
+        return (
+            <div className="col-md-6 col-md-offset-3">
+                <h3>Staff PAGE</h3>
+                {users.loading && <em>Loading users...</em>}
+                {users.error && <span className="text-danger">ERROR: {users.error}</span>}
+                {users.items &&
+                    <ul>
+                        {users.items.map((user, index) =>
+                            <li key={user.id}>
+                                {user.firstName + ' ' + user.lastName + ' ' + user.isAdmin}
+                                {
+                                    user.deleting ? <em> - Deleting...</em>
+                                    : user.deleteError ? <span className="text-danger"> - ERROR: {user.deleteError}</span>
+                                    : <span> - <a onClick={this.handleDeleteUser(user.id)}>Delete</a></span>
+                                }
+                            </li>
+                        )}
+                    </ul>
+                }
+            </div>
+        );
+    }
+}
+
+function mapState(state) {
+    const { users, authentication } = state;
+    const { user } = authentication;
+    return { user, users };
+}
+
+const actionCreators = {
+    getUsers: userActions.getAll,
+    deleteUser: userActions.delete
+}
+
+const connectedHomePage = connect(mapState, actionCreators)(StaffPage);
+export { connectedHomePage as StaffPage };
